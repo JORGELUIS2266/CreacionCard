33a47c832598ba7fcf11df93c0a3d63ffd8e1e87
JORGELUIS2266 <yo.hemati2322@gmail.com>
2025-03-24 21:56:48 -0600

    agregando nuevo componente.vue

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -3,12 +3,13 @@ import Row from './Row.vue';
 </script>
 
 <template>
-    <center><h1>Explorando el Mundo de la Programación Web</h1></center>
-  
+    <center>
+        <h1>Explorando el Mundo de la Programación Web
+        </h1>
+    </center>
     <div class="container">
       <Row/>
     </div>
-    
   </template>
 
   
